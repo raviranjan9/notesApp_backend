2deeb28f4ab362515e596bb5907255a7275acd15
raviranjan <raviranjancp@gmail.com>
2023-02-02 17:49:32 +0530

    backendFirstCommit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express'); 
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+dotenv.config({path: './config.env'});
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const userRouter = require('./routes/users.js');
+const noteRouter = require('./routes/notes.js');
+
+app.use(userRouter);
+app.use(noteRouter);
+
+const URL = process.env.DB;
+
+mongoose.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true});
+
+app.listen(process.env.PORT, (err) => {
+    if(err)
+        console.log(err);
+    else
+        console.log(`Server is listening at Port: ${process.env.PORT}`);
+});
